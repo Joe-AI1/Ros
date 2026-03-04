35670d972c98e49f79755fbf1b5d3e6d6cc3d517
Joe-AI1 <66011545@kmitl.ac.th>
2026-03-04 16:04:43 +0700

    Update PIDTune.sh

--- robot_ws/PIDTune.sh
+++ robot_ws/PIDTune.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "FL", kp: 2.0, ki: 1.5, kd: 0.02}"
+ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "FL", kp: 3.0, ki: 1.5, kd: 0.02}"
 ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "FR", kp: 5.0, ki: 1.7, kd: 0.04}"
-ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "RL", kp: 4.5, ki: 1.7, kd: 0.02}"
+ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "RL", kp: 5.5, ki: 1.7, kd: 0.02}"
 ros2 service call /ired/pid/tuning ired_msgs/srv/PIDTuning "{motor: "RR", kp: 5.0, ki: 1.7, kd: 0.04}"
 echo "Success Tune"
